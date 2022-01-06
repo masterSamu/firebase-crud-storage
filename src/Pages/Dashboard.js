e85bc9715cb6a3da3a6b2186a229ed762a3895cd
Samu <samu.uunonen@student.hamk.fi>
2022-01-06 17:46:39 +0200

    Setting up Router
    Set up router and dashboard.

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>MenuItems</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
